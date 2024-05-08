226a58d88bf074bc0df9b2c7408621924e522a18
Muhammad Bin Azhar <64974141+tipu-zlatan@users.noreply.github.com>
2024-05-08 14:34:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask import jsonify
 from flask import request
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
