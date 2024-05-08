2ca05f5aa01a0f9e1a3762ea6da71f9a73a16932
Muhammad Bin Azhar <64974141+tipu-zlatan@users.noreply.github.com>
2024-05-08 11:53:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ def hello():
 
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
-    return jsonify({'ip': request.remote_addr}), 200
+    if request.headers.getlist("X-Forwarded-For"):
+       ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+       ip = request.remote_addr
+    return jsonify({'ip': ip}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, ssl_context='adhoc')
